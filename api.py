85c0ee9621546fd83c8fbcb967f5a7cebbaacbaf
Unmesh28 <unmesh.dabhade@gmail.com>
2022-02-13 17:20:11 +0530

    AWS Polly Added

--- api.py
+++ api.py
@@ -3,6 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile, Form, BackgroundTasks
 from fastapi.responses import FileResponse
 import uvicorn
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import os
+import sys
+import subprocess
+from tempfile import gettempdir
 
 app = FastAPI()
 
@@ -14,6 +21,44 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+# Create a client using the credentials and region defined in the [adminuser]
+# section of the AWS credentials file (~/.aws/credentials).
+session = Session(profile_name="adminuser")
+polly = session.client("polly")
+
+
+def convertTextToSpeec(text):
+    try:
+    # Request speech synthesis
+        response = polly.synthesize_speech(Text="Hello world!", OutputFormat="mp3",
+                                            VoiceId="Joanna")
+    except (BotoCoreError, ClientError) as error:
+        # The service returned an error, exit gracefully
+        print(error)
+        sys.exit(-1)
+
+    # Access the audio stream from the response
+    if "AudioStream" in response:
+        # Note: Closing the stream is important because the service throttles on the
+        # number of parallel connections. Here we are using contextlib.closing to
+        # ensure the close method of the stream object will be called automatically
+        # at the end of the with statement's scope.
+            with closing(response["AudioStream"]) as stream:
+                output = os.path.join(gettempdir(), "speech.mp3")
+                
+
+            try:
+                # Open a file for writing the output as a binary stream
+                with open(output, "wb") as file:
+                    file.write(stream.read())
+                    return 200
+            except IOError as error:
+                # Could not write to file, exit gracefully
+                print(error)
+                return 400
+                sys.exit(-1)
+
+
 @app.get('/')
 async def root():
     return {'hello': 'world'}
@@ -21,4 +66,4 @@ async def root():
 
 @app.get('textToSpeech/')
 async def textToSpeech(text: str):
-    return text 
+    return text
