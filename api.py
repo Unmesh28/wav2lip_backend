25bf5d9b4c71e1912bfae7913db1db77f3f3ea7e
Unmesh28 <unmesh.dabhade@gmail.com>
2022-02-14 08:47:09 +0530

    Updates

--- api.py
+++ api.py
@@ -74,7 +74,7 @@ async def wav2lip(file: UploadFile):
         content = await file.read()  # async read
         await out_file.write(content)  # async write
 
-    subprocess.run(["python", "inference.py", "--checkpoint_path", "checkpoints/wav2lip_gan.pth", "--face", "video.mp4", "--audio" "speech.wav"])
+    subprocess.run(["python", "/home/ubuntu/Wav2Lip/inference.py", "--checkpoint_path", "checkpoints/wav2lip_gan.pth", "--face", "video.mp4", "--audio" "speech.wav"])
     #print("The exit code was: %d" % list_files.returncode)  
     #python inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face "video.mp4" --audio "/content/sample_data/input_audio.wav"
 
